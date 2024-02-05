04c2cdb12b309b9fcc204ad948ff7561a0238415
Linfantee <145344308+Linfantee@users.noreply.github.com>
2024-02-05 13:41:49 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace Ej1.Fechas;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        DateTime fechaActual = DateTime.Now;
+        
+       DateTimeFormatInfo dateTimeFormatInfo = CultureInfo.GetCultureInfo("es-SP").DateTimeFormat;
+        Type tipo = dateTimeFormatInfo.GetType();
+        PropertyInfo[] propiedadesFormatos = tipo.GetProperties();
+        foreach (var propiedad in propiedadesFormatos)
+        {
+            if (propiedad.Name.Contains("Patern"))
+            {
+                string formatofecha = propiedad.GetValue(dateTimeFormatInfo, null).ToString();
+                Console.WriteLine(propiedad.Name + ": " + formatofecha + " " +
+                                  fechaActual.ToString(formatofecha));
+            }
+        }
+        Console.WriteLine("-----------------");
+        Console.WriteLine("Formato fecha-tiempo: " + fechaActual.ToString("s"));
+
+
+
+
+
+
+    }
+
+
+
+}
